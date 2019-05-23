53b04c2be77d56a8ab7cb1930fd5b6a0f1b33722
rmit-s3626050-Trung-Pham <s3626050@student.rmit.edu.au>
2019-05-23 22:51:04 +1000

    trungpt

--- poffolio/src/Header.js
+++ poffolio/src/Header.js
@@ -9,25 +9,27 @@ const Header = ({children}) => {
         marginBottom: 30,
         color:'red',
     };
-    var style2 = {
-        float: 'right'
-    }
-    var bgColors = { "Default": "#81b71a",
-                    "Blue": "#00B1E1",
-                    "Cyan": "#37BC9B",
-                    "Green": "#8CC152",
-                    "Red": "#E9573F",
-                    "Yellow": "#F6BB42",
-    };
+    // var style2 = {
+    //     float: 'right'
+    // }
+    // var bgColors = { "Default": "#81b71a",
+    //                 "Blue": "#00B1E1",
+    //                 "Cyan": "#37BC9B",
+    //                 "Green": "#8CC152",
+    //                 "Red": "#E9573F",
+    //                 "Yellow": "#F6BB42",
+    // };
+    // var surveyTitle = ""
+    
     return (
         <div className="mainHeader">
             <h3 style={style}><Link to="/create_survey">Add Question</Link></h3>
             <h3 style={style}><Link to="/review_question">Review Survey</Link></h3>
             <h3 style={style}><Link to="/review_survey">Review Answer</Link></h3>
-            <div style={Object.assign({backgroundColor: bgColors.Blue}, style, style2)} >
-                <input type="text"></input>
+            {/* <div style={Object.assign({backgroundColor: bgColors.Blue}, style, style2)} >
+                <input id="surveyTitle" type="text"></input>
                 <button type="submit">New Survey</button>
-            </div>
+            </div> */}
             {children}
         </div>
     )
