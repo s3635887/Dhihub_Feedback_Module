ea23fc745f2e97daaa5c3df506476d987573341c
rmit-s3635887-pavan-w <37763559+s3635887@users.noreply.github.com>
2019-06-16 11:59:08 +1000

    Delete slack3.py

--- BackEnd/slack3.py
+++
@@ -1,5 +0,0 @@
-import sys
-from slacker import Slacker
-slack = Slacker('xoxb-575060474148-585358061488-uSAy4gzktu8jztx3154DzKdr')
-message = "Hello Everyone"
-slack.chat.post_message('rmit-only',message)
